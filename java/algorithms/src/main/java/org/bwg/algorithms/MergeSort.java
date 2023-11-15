9f6178f594b2ac3cb4534380a5118fbbc44e36e8
Brett Drainer <brettdrainer@yahoo.com>
2023-11-15 10:09:32 -0800

    Initial commit

---
+++ java/algorithms/src/main/java/org/bwg/algorithms/MergeSort.java
@@ -0,0 +1,73 @@
+package org.bwg.algorithms;
+
+public class MergeSort {
+
+    public void sort(String[] array) {
+        assert array != null;
+        sort(array, 0, array.length - 1);
+    }
+
+    void sort(String[] arr, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+    void merge(String[] arr, int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        String[] L = new String[n1];
+        String[] R = new String[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].compareToIgnoreCase(R[j]) <= 0) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
+
---
+++ java/algorithms/src/test/java/org/bwg/algorithms/MergeSortTest.java
@@ -0,0 +1,59 @@
+package org.bwg.algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MergeSortTest {
+
+    MergeSort sorter = new MergeSort();
+
+    @Test
+    void shouldThrowOnNullArray() {
+        assertThrows(AssertionError.class, () -> sorter.sort(null));
+    }
+
+    @Test
+    void shouldSortEmptyArray() {
+        sortAndAssert(new String[0], new String[0]);
+    }
+
+    @Test
+    void shouldSortSingleValueArray() {
+        sortAndAssert(new String[]{"a"}, new String[]{"a"});
+    }
+
+    @Test
+    void shouldSortMixedCaseArray() {
+        sortAndAssert(new String[]{"a", "A"}, new String[]{"a", "A"});
+        sortAndAssert(new String[]{"A", "a"}, new String[]{"A", "a"});
+    }
+
+    @Test
+    void shouldSortLettersArray() {
+        sortAndAssert(
+                new String[]{"e", "d", "c", "b", "a"},
+                new String[]{"a", "b", "c", "d", "e"});
+    }
+
+    @Test
+    void shouldSortWordsArray() {
+        sortAndAssert(
+                new String[]{"eggs", "dough", "carrot", "bread", "apple"},
+                new String[]{"apple", "bread", "carrot", "dough", "eggs"});
+    }
+
+    @Test
+    void shouldSortMultiCaseWords() {
+        var expected = new String[]{"APPLE", "Bread", "caRRot", "Dough", "eggS"};
+        var array = new String[]{"eggS", "Dough", "caRRot", "Bread", "APPLE"};
+        sortAndAssert(array, expected);
+    }
+
+    private void sortAndAssert(String[] array, String[] expected) {
+        sorter.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+}
